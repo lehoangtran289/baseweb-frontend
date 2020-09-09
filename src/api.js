7858c81bfb6bebb1ceb6f717627fdf2074b807a1
Tran Le Hoang <lehoangtran289@gmail.com>
2020-09-09 17:06:18 +0700

    init call apis

---
+++ src/api.js
@@ -0,0 +1,103 @@
+import axios from "axios";
+import {API_URL} from "./config/config";
+import {failed} from "./action";
+
+
+export const authPost = (dispatch, token, url, body) => {
+  return fetch(API_URL + url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "X-Auth-Token": token,
+    },
+    body: JSON.stringify(body),
+  });
+};
+
+export const authPut = (dispatch, token, url, body) => {
+  return fetch(API_URL + url, {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+      "X-Auth-Token": token,
+    },
+    body: JSON.stringify(body),
+  });
+};
+
+export const authGet = (dispatch, token, url) => {
+  return fetch(API_URL + url, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "X-Auth-Token": token,
+    },
+  }).then(
+    (res) => {
+      if (!res.ok) {
+        if (res.status === 401) {
+          dispatch(failed());
+        }
+        return null;
+      }
+      return res.json();
+    },
+    (error) => {
+      console.log(error);
+    }
+  )
+}
+
+export const authDelete = (dispatch, token, url) => {
+  return fetch(API_URL + url, {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+      "X-Auth-Token": token,
+    },
+  }).then(
+    (res) => {
+      if (!res.ok) {
+        dispatch(failed());
+        throw Error("Unauthorized");
+      }
+      return true;
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+};
+
+// export default {
+//   getMenu: (dispatch, token) => {
+//     return authGet(dispatch, token, "/menu");
+//   }
+// }
+
+export const axiosPost = (dispatch, token, url, data) => {
+  return axios.post(API_URL + url, data, {
+    headers: {
+      "content-type": "application/json",
+      "X-Auth-Token": token,
+    },
+  });
+};
+
+export const axiosGet = (dispatch, token, url) => {
+  return axios.get(API_URL + url, {
+    headers: {
+      "content-type": "application/json",
+      "X-Auth-Token": token,
+    },
+  });
+};
+
+export const axiosPut = (dispatch, token, url, data) => {
+  return axios.put(API_URL + url, data, {
+    headers: {
+      "content-type": "application/json",
+      "X-Auth-Token": token,
+    },
+  });
+};
