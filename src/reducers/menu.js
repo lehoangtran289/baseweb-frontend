901fdc00bea8e0352fdf0760b53717621916a1f7
Tran Le Hoang <lehoangtran289@gmail.com>
2020-09-09 17:07:19 +0700

    init reducers

---
+++ src/reducers/menu.js
@@ -0,0 +1,39 @@
+import {MENU_REQUEST_FAILURE, MENU_REQUEST_SUCCESS, MENU_REQUESTING, SELECTED_FUNCTION_UPDATE} from "../action";
+
+const menu = (state = {
+  menu: new Set(),
+  isRequesting: false,
+  isMenuGot: false,
+  selectedFunction: null,
+}, action) => {
+
+  switch (action.type) {
+    case MENU_REQUESTING:
+      return Object.assign({}, state, {isRequesting: true})
+
+    case MENU_REQUEST_SUCCESS:
+      return Object.assign({}, state, {
+        menu: new Set(action.menu),
+        isMenuGot: true,
+        isRequesting: false,
+      })
+
+    case MENU_REQUEST_FAILURE:
+      return Object.assign({}, state, {
+        isRequesting: false,
+        isMenuGot: false,
+      })
+
+    case SELECTED_FUNCTION_UPDATE:
+      return Object.assign({}, state, {
+        selectedFunction: action.selectedFunction,
+      });
+
+    default:
+      return state;
+
+  }
+
+}
+
+export default menu;
