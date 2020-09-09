a996f657ffe729a00cbb4e24e97c8ee072dbee71
Tran Le Hoang <lehoangtran289@gmail.com>
2020-09-09 10:06:50 +0700

    init routes, will impl redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Router} from "react-router";
+import {Slide, ToastContainer} from "react-toastify";
+import Routes from "./Routes";
 
 function App() {
     return (
-        <div>
-
-        </div>
+        <Router>
+            <Routes/>
+            <ToastContainer
+                position="bottom-left"
+                transition={Slide}
+                autoClose={2000}
+                hideProgressBar={true}
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </Router>
     );
 }
 
