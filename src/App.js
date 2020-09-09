a003017b94c1186087c93c482c7417369490dd4d
Tran Le Hoang <lehoangtran289@gmail.com>
2020-09-09 21:10:39 +0700

    fix import Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Router} from "react-router";
 import {Slide, ToastContainer} from "react-toastify";
 import Routes from "./Routes";
+import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
-    return (
-        <Router>
-            <Routes/>
-            <ToastContainer
-                position="bottom-left"
-                transition={Slide}
-                autoClose={2000}
-                hideProgressBar={true}
-                newestOnTop
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes/>
+      <ToastContainer
+        position="bottom-left"
+        transition={Slide}
+        autoClose={2000}
+        hideProgressBar={true}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Router>
+  );
 }
 
 export default App;
